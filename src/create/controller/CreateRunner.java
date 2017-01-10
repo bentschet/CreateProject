4357cee5865008617f8881ed5926637d16fdc02e
bentschet <btsc0323@740s-pl-126.local>
2017-01-10 13:55:20 -0700

    saved again
    asdfasdf

--- src/create/controller/CreateRunner.java
+++ src/create/controller/CreateRunner.java
@@ -3,8 +3,8 @@ package create.controller;
 public class CreateRunner 
 {
 	public static void main (String [ ] args)
-		{
-		CreateController createAppController = new CreateController();
-		createAppController.start();
-		}
+	{
+	CreateController createAppController = new CreateController();
+	createAppController.start();
+	}
 }
