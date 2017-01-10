7fb6bc3406e4b178c17501f9ab7d8c9362266a5c
bentschet <btsc0323@740s-pl-126.local>
2017-01-10 12:41:01 -0700

    saved the files again.
    dasds

--- src/create/controller/CreateController.java
+++ src/create/controller/CreateController.java
@@ -2,4 +2,10 @@ package create.controller;
 
 public class CreateController {
 
+	public void start() 
+	{
+		
+		
+	}
+
 }
--- src/create/controller/CreateRunner.java
+++ src/create/controller/CreateRunner.java
@@ -1,5 +1,10 @@
 package create.controller;
 
-public class CreateRunner {
-
+public class CreateRunner 
+{
+	public static void main (String [ ] args)
+		{
+		CreateController createAppController = new CreateController();
+		createAppController.start();
+		}
 }
