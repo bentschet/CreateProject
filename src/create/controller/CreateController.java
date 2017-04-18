2a80832b85c5ea8a44f1ee1d91c756ccbeb62842
bentschet <btsc0323@740s-pl-126.local>
2017-04-18 12:54:18 -0600

    aaaa

--- src/create/controller/CreateController.java
+++ src/create/controller/CreateController.java
@@ -1,13 +1,23 @@
 package create.controller;
 
-public class CreateController {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class CreateController 
+{
+	private String LowercasePattern = "(?=.*[a-z])";
+	private String DigitPattern = "(?=.*d)";
+	private String SpecialCharPattern = "(?=.*[@#$%])";
+	private String UppercasePattern = "((?=.*[A-Z])";
+	
 	public void start() 
 	{
-		
-		
+				
 	}
 	
-	
+	public void checkLowercase()
+	{
+		
+	}
 
 }
